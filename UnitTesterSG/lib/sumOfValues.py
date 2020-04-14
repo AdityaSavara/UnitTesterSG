83b5ba11b4bcee03a9a2369627de1a9176790c39
AdityaSavara <39929571+AdityaSavara@users.noreply.github.com>
2020-04-14 02:24:36 -0400

    Update sumOfValues.py

--- UnitTesterSG/lib/sumOfValues.py
+++ UnitTesterSG/lib/sumOfValues.py
@@ -1,10 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Nov 28 11:22:59 2017
-
-@author: tienhung2501
-"""
-
 def sum_of_elements(value_list=[]):
     sum_elements=0
     for i in range(0,len(value_list)):
