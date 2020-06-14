9eb3b7bc48f6e475d0f74bdc47a6ca463eab822f
AdityaSavara <39929571+AdityaSavara@users.noreply.github.com>
2020-06-14 14:37:48 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ URL = 'https://github.com/AdityaSavara/UnitTesterSG'
 EMAIL = 'AditySavara2008@u.northwestern.edu'  
 AUTHOR = 'Aditya Savara'
 REQUIRES_PYTHON = '>=3.0.0'
-VERSION = '5.0.2'
+VERSION = '5.0.3'
 LICENSE = 'BSD-3-Clause'
 
 # What packages are required for this module to be executed?
